244ef16d3f7c64b6919d29e6f110a5539798b370
Vincent Shahriyari <vincent@wechselpilot.com>
2024-12-23 17:36:21 +0100

    Added Tags Filter

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,21 @@
 const filterMenu = document.querySelector(".filter");
 const filterListcreator = document.querySelector(".creators");
 const filterListDevice = document.querySelector(".devices");
+const filterListtagg = document.querySelector(".taggs");
 const filterButtons = filterMenu.querySelectorAll(".filter-btn");
 const recipies = document.querySelectorAll(".recipieFilterSelector");
+
+const filterTypeEnum = {
+    creator: "creator",
+    device: "device",
+    tagg: "tagg"
+}
+
+
+
 let activecreator = "all";
 let activeDevice = "all";
+let activetagg = "all";
 let conferenceIndex = 0;
 
 recipies.forEach((conference) => {
@@ -13,22 +24,30 @@ recipies.forEach((conference) => {
 
 filterButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        let iscreator = false;
-        if (e.target.classList.contains("filter-btn-auth")) {
-            let confCategory = e.target.getAttribute("data-filter-creator");
-            activecreator = confCategory;
-            iscreator = true;
-        } else {
-            let confCategory = e.target.getAttribute("data-filter-device");
-            activeDevice = confCategory;
+        let filterType = null;
+        switch (e.target.classList.values().find(x => x.includes("filter-btn-"))){
+            case "filter-btn-auth":
+                activecreator = e.target.getAttribute("data-filter-creator");
+                filterType = filterTypeEnum.creator;
+                break;
+            case "filter-btn-device":
+                activeDevice = e.target.getAttribute("data-filter-device");
+                filterType = filterTypeEnum.device;
+                break;
+            case "filter-btn-taggs":
+                activetagg = e.target.getAttribute("data-filter-tagg")
+                filterType = filterTypeEnum.tagg;
+                break;
+            default:
+                return;
         }
         if (!document.startViewTransition) {
-            updateActiveButton(e.target, iscreator);
+            updateActiveButton(e.target, filterType);
             filterEvents();
         }
 
         document.startViewTransition(() => {
-            updateActiveButton(e.target, iscreator);
+            updateActiveButton(e.target, filterType);
             filterEvents();
         });
     });
@@ -36,7 +55,20 @@ filterButtons.forEach((button) => {
 
 
 function updateActiveButton(newButton, iscreator) {
-    let list = iscreator ? filterListcreator : filterListDevice
+    let list = null;
+    switch (iscreator) {
+        case filterTypeEnum.creator:
+            list = filterListcreator;
+            break;
+        case filterTypeEnum.device:
+            list = filterListDevice;
+            break;
+        case filterTypeEnum.tagg:
+            list = filterListtagg;
+            break;
+        default:
+            return;
+    }
     list.querySelector(".active").classList.remove("active");
     newButton.classList.add("active");
 }
@@ -45,10 +77,13 @@ function filterEvents() {
     recipies.forEach((conference) => {
         // get each conferences category
         let recipiecreators = (conference.getAttribute("data-creator") || "").split(",");
-        let recipieDevice = conference.getAttribute("data-device");
+        let recipieDevice = conference.getAttribute("data-device" || "");
+        let recipietagg = (conference.getAttribute("data-tagg") || "").split(",");
 
         // check if that category matches with the filter
-        if ((activecreator === "all" || recipiecreators.includes(activecreator)) && (activeDevice === "all" || activeDevice === recipieDevice)) {
+        if ((activecreator === "all" || recipiecreators.includes(activecreator))
+            && (activeDevice === "all" || activeDevice === recipieDevice)
+            && (activetagg === "all" || recipietagg.includes(activetagg))) {
             conference.removeAttribute("hidden");
         } else {
             conference.setAttribute("hidden", "");
